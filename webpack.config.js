3914d581cf63910afaae19f2c53b29bdb1436165
ArtoriasDelAbismo <jerojournade1@gmail.com>
2023-03-22 07:19:09 -0300

    add css loader/plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-const { dirname } = require('path');
+const { dirname } = require('path');//?
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -19,8 +21,24 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ]
             }
         
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ]
 }
